1af4db551ea6b3aa581fab251174be0b722b6e12
SJ <suresh@vicarasolutions.com>
2022-01-17 09:44:04 -0500

    See issue #18: First pass at subsystem

--- pants-plugins/experimental/pyoxidizer/register.py
+++ pants-plugins/experimental/pyoxidizer/register.py
@@ -1,10 +1,10 @@
-
 from experimental.pyoxidizer.rules import rules as pyoxidizer_rules
+from experimental.pyoxidizer import subsystem
 from experimental.pyoxidizer.target_types import PyOxidizerTarget
 
 
 def rules():
-    return [*pyoxidizer_rules()]
+    return [*pyoxidizer_rules(), *subsystem.rules()]
 
 
 def target_types():
